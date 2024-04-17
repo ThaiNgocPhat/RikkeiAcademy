10a90985600c155ff254796cbe291f25b06d55ee
Thai Ngoc Phat <thaingocphat@Gaumap.local>
2024-04-17 08:39:51 +0700

    push

--- Project Moduel 1/Overview/Overview.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById('myVideo');
-video.controls = false;
-video.addEventListener('click', function() {
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
-});
